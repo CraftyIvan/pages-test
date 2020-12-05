acbaf15bdaee22d3efad60ff8a5c057ab0805412
Ivan Kolundžija <ivankolundzija@gmail.com>
2020-12-05 03:32:03 +0100

    Adding everything for the initial learning page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  async componentDidMount() {
+    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usersJson = await usersResponse.json();
+    this.setState({ monsters: usersJson });
+  }
+
+  // Learning comment: this happens because arrow functions' this is not
+  // set by the calling function, but the calling function's this is used
+  // instead. if a calls b and b calls handleChange, this is the same as
+  // this in b, which is set by a.
+  handleChange = (e) => {
+    this.setState( { searchField: e.target.value });
+  };
+
+  render() {
+
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Galore</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={ filteredMonsters } />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export function SearchBox({ placeholder, handleChange }) {
+    return (
+        <input
+            type='search'
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    );
+}
